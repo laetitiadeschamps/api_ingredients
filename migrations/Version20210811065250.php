cabd2ce859ec9a00b3464b361723002e71c4db3f
Laetitia Deschamps <name>
2021-08-12 14:55:15 +0200

    migrations update category_id

--- migrations/Version20210811065250.php
+++ migrations/Version20210811065250.php
@@ -21,6 +21,7 @@ final class Version20210811065250 extends AbstractMigration
     {
         $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
         , password VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
 
     }
 
